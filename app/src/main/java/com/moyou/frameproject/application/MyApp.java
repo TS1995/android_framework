8ea036b62a71d67860f5bfb68fa1e70a862b4e91
584076388@qq.com <pyx1994512>
2017-08-11 11:31:05 +0800

    更新依赖

--- app/src/main/java/com/moyou/frameproject/application/MyApp.java
+++ app/src/main/java/com/moyou/frameproject/application/MyApp.java
@@ -10,6 +10,6 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
+        MyAppInitSDK.initLibrary(this);
     }
 }
--- app/src/main/java/com/moyou/frameproject/application/MyAppInitSDK.java
+++ app/src/main/java/com/moyou/frameproject/application/MyAppInitSDK.java
@@ -2,8 +2,15 @@ package com.moyou.frameproject.application;
 
 import android.content.Context;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.model.GlideUrl;
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
+
+import java.io.InputStream;
+
+import okhttp3.OkHttpClient;
 
 /**
  * Created by Administrator on 2017/8/11 0011.
@@ -13,9 +20,11 @@ public class MyAppInitSDK {
 
     /**
      * 初始化一些第三方
-     * @param context
+     *
+     * @param mContext
      */
-    public static void initLibrary(Context context) {
+    public static void initLibrary(Context mContext) {
         Logger.addLogAdapter(new AndroidLogAdapter());
+        Glide.get(mContext).register(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(new OkHttpClient()));
     }
 }
